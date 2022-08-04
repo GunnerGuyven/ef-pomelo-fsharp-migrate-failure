80cdb93e20d75f7ac653175b8f37a67cf2a992c8
Guyven <guyven@pm.me>
2022-08-04 13:42:01 -0400

    initial

---
+++ DBModel.fs
@@ -0,0 +1,31 @@
+module DBModel
+
+open Microsoft.EntityFrameworkCore
+open EntityFrameworkCore.FSharp.Extensions
+open System
+open Microsoft.Extensions.Logging
+
+[<CLIMutable>]
+type Blog =
+    { Id: int
+      Title: string
+      Content: string }
+
+type DBModelContext() =
+    inherit DbContext()
+
+    [<DefaultValue>]
+    val mutable blogs: DbSet<Blog>
+
+    member this.Blogs
+        with get () = this.blogs
+        and set v = this.blogs <- v
+
+    override _.OnModelCreating builder = builder.RegisterOptionTypes()
+
+    override _.OnConfiguring options =
+        options.UseMySql(
+            "server=localhost;user=testdb;password=abc123;database=testdb",
+            MariaDbServerVersion(Version(10, 8, 3))
+        )
+        |> ignore
